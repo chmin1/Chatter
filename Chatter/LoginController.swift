c5b65b19657f5c30be2a17b30a30923963af74f2
Chevy-Personal Mac <chavaneminto@gmail.com>
2017-12-08 16:43:54 -0500

    Users can sign up with username, email and password

--- Chatter/LoginController.swift
+++ Chatter/LoginController.swift
@@ -9,13 +9,51 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white])
+        passwordField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white])
 
-        // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 15;
+        signUpButton.layer.cornerRadius = 15;
+        usernameField.layer.cornerRadius = 15;
+        passwordField.layer.cornerRadius = 15;
+        
+        usernameField.layer.masksToBounds = true;
+        passwordField.layer.masksToBounds = true;
+        loginButton.layer.masksToBounds = true;
+        signUpButton.layer.masksToBounds = true;
+        
+        usernameField.layer.borderColor = UIColor.white.cgColor;
+        passwordField.layer.borderColor = UIColor.white.cgColor;
+        loginButton.layer.borderColor = UIColor.white.cgColor;
+        signUpButton.layer.borderColor = UIColor.white.cgColor;
+        
+        usernameField.layer.borderWidth = 3.0;
+        passwordField.layer.borderWidth = 3.0;
+        loginButton.layer.borderWidth = 3.0;
+        signUpButton.layer.borderWidth = 3.0;
+        
     }
-
+    
+    @IBAction func onLogin(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onScreenTap(_ sender: Any) {
+        view.endEditing(true);
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Chatter/SignUpViewController.swift
+++ Chatter/SignUpViewController.swift
@@ -7,15 +7,118 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    @IBOutlet weak var createButton: UIButton!
+    
+    var alertController: UIAlertController!
+    var alertError: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        usernameField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white]);
+        emailField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white]);
+        passwordField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white]);
+        confirmPasswordField.attributedPlaceholder = NSAttributedString(string: "Confirm Password", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white]);
+        
+        cancelButton.layer.cornerRadius = 15;
+        createButton.layer.cornerRadius = 15;
+        usernameField.layer.cornerRadius = 15;
+        emailField.layer.cornerRadius = 15;
+        passwordField.layer.cornerRadius = 15;
+        confirmPasswordField.layer.cornerRadius = 15;
+        
+        usernameField.layer.masksToBounds = true;
+        emailField.layer.masksToBounds = true;
+        passwordField.layer.masksToBounds = true;
+        confirmPasswordField.layer.masksToBounds = true;
+        cancelButton.layer.masksToBounds = true;
+        createButton.layer.masksToBounds = true;
+        
+        
+        usernameField.layer.borderColor = UIColor.white.cgColor;
+        emailField.layer.borderColor = UIColor.white.cgColor;
+        passwordField.layer.borderColor = UIColor.white.cgColor;
+        confirmPasswordField.layer.borderColor = UIColor.white.cgColor;
+        cancelButton.layer.borderColor = UIColor.white.cgColor;
+        createButton.layer.borderColor = UIColor.white.cgColor;
+        
+        usernameField.layer.borderWidth = 3.0;
+        emailField.layer.borderWidth = 3.0;
+        passwordField.layer.borderWidth = 3.0;
+        confirmPasswordField.layer.borderWidth = 3.0;
+        cancelButton.layer.borderWidth = 3.0;
+        createButton.layer.borderWidth = 3.0;
+        
     }
-
+    
+    @IBAction func onScreenTap(_ sender: Any) {
+        view.endEditing(true);
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onCreateUser(_ sender: Any) {
+        if passwordField.text == confirmPasswordField.text {
+            let newUser = PFUser()
+            
+            newUser.username = usernameField.text
+            newUser.email = emailField.text
+            newUser.password = passwordField.text
+            
+            newUser.signUpInBackground { (success: Bool, error: Error?) in
+                if let error = error{
+                    print("User log in failed: \(error.localizedDescription)")
+                    self.alertError = error.localizedDescription
+                    self.isSignedUp()
+                } else {
+                    print("User logged in successfully")
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        } else {
+            self.alertController = UIAlertController(title: "Sign Up Error", message: "Passwords Don't Match", preferredStyle: .alert)
+            
+            let tryAgain = UIAlertAction(title: "Try Again", style: .cancel) { (action) in
+                self.usernameField.text = ""
+                self.passwordField.text = ""
+                self.emailField.text = ""
+            }
+            alertController.addAction(tryAgain)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func isSignedUp() {
+        self.alertController = UIAlertController(title: "Sign Up Error", message: alertError, preferredStyle: .alert)
+        
+        //try to connect again
+        let tryAgain = UIAlertAction(title: "Try Again", style: .cancel) { (action) in
+            self.usernameField.text = ""
+            self.passwordField.text = ""
+            self.emailField.text = ""
+        }
+        
+        // add action to alertController
+        alertController.addAction(tryAgain)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

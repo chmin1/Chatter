a6b14c9a74264750c1fe1d9b55095fa56d4e1009
Chevy-Personal Mac <chavaneminto@gmail.com>
2017-12-08 21:10:48 -0500

    User can now write and save messages

--- Chatter/chatViewController.swift
+++ Chatter/chatViewController.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import Parse
 
 class chatViewController: UIViewController {
-
+    
+    @IBOutlet weak var chatField: UITextField!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        chatField.attributedPlaceholder = NSAttributedString(string: "Compose A Message...", attributes: [NSAttributedStringKey.foregroundColor : UIColor.white]);
+        
+        chatField.layer.cornerRadius = 15;
+        sendButton.layer.cornerRadius = 15;
+        
+        chatField.layer.masksToBounds = true;
+        sendButton.layer.masksToBounds = true;
+        
+        chatField.layer.borderColor = UIColor.white.cgColor;
+        sendButton.layer.borderColor = UIColor.white.cgColor;
+        
+        chatField.layer.borderWidth = 3.0;
+        sendButton.layer.borderWidth = 3.0;
+        
     }
-
+    
+    @IBAction func onSend(_ sender: Any) {
+        if chatField.text != "" {
+            let chatMessage = PFObject(className: "Message");
+            chatMessage["Text"] = chatField.text
+            
+            chatMessage.saveInBackground(block: { (success, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("The message was saved!");
+                    self.chatField.text = "";
+                }
+            })
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
